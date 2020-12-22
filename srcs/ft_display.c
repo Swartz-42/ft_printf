604a4cf7faa3d63fe44aa7c3919e33d621b43f1d
Alexandre Ducas <aducas@z1r1p2.42lyon.fr>
2020-12-22 11:08:37 +0100

    push

--- srcs/ft_display.c
+++ srcs/ft_display.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_display.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aducas <aducas@student.le-101.fr>          +#+  +:+       +#+        */
+/*   By: aducas <aducas@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 17:40:10 by lrobert           #+#    #+#             */
-/*   Updated: 2020/02/21 16:29:23 by aducas           ###   ########lyon.fr   */
+/*   Updated: 2020/12/21 16:39:59 by aducas           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,18 @@ static void	ft_display_pos(t_tab *tpf, long int val)
 	}
 	else
 	{
-		ft_putnbr_fd(val, 1);
-		ft_putnchar_fd('0', tpf->nbzero, 1);
-		ft_putnchar_fd(' ', tpf->nbspace, 1);
+		if (tpf->fprecision == TRUE)
+		{
+			ft_putnchar_fd('0', tpf->nbzero, 1);
+			ft_putnbr_fd(val, 1);
+			ft_putnchar_fd(' ', tpf->nbspace, 1);
+		}
+		else
+		{
+			ft_putnbr_fd(val, 1);
+			ft_putnchar_fd('0', tpf->nbzero, 1);
+			ft_putnchar_fd(' ', tpf->nbspace, 1);
+		}
 	}
 }
 
@@ -39,10 +48,20 @@ static void	ft_display_neg(t_tab *tpf, long int val)
 	}
 	else
 	{
-		ft_putchar_fd('-', 1);
-		ft_putnchar_fd('0', tpf->nbzero, 1);
-		ft_putnbr_fd(-val, 1);
-		ft_putnchar_fd(' ', tpf->nbspace, 1);
+		if (tpf->fprecision == TRUE)
+		{
+			ft_putchar_fd('-', 1);
+			ft_putnchar_fd('0', tpf->nbzero, 1);
+			ft_putnbr_fd(-val, 1);
+			ft_putnchar_fd(' ', tpf->nbspace, 1);
+		}
+		else
+		{
+			ft_putchar_fd('-', 1);
+			ft_putnchar_fd('0', tpf->nbzero, 1);
+			ft_putnbr_fd(-val, 1);
+			ft_putnchar_fd(' ', tpf->nbspace, 1);
+		}
 	}
 }
 
@@ -72,8 +91,8 @@ void		ft_display_ui(t_tab *tpf, unsigned int val)
 	}
 	else
 	{
-		ft_putnbr_base(val, BASE10);
 		ft_putnchar_fd('0', tpf->nbzero, 1);
+		ft_putnbr_base(val, BASE10);
 		ft_putnchar_fd(' ', tpf->nbspace, 1);
 	}
 }
@@ -82,6 +101,12 @@ void		ft_display(t_tab *tpf, int val, int sign)
 {
 	if (sign == 0)
 		ft_display_pos(tpf, val);
-	else
+	else if (sign == 1)
 		ft_display_neg(tpf, val);
+	else
+	{
+		ft_putnchar_fd('0', tpf->nbzero, 1);
+		ft_putnbr_fd(val, 1);
+		ft_putnchar_fd(' ', tpf->nbspace, 1);
+	}
 }
